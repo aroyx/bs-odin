53ddbfaced2f311ab2401ac049869948f5745e77
Ankush Roy <ankush3411111@gmail.com>
2026-06-12 15:23:36 +0530

    Misc: Made the argument to be taken in any order

--- build.sh
+++ build.sh
@@ -3,77 +3,50 @@
 # Usage: 
 # `./build.sh` - run the client
 #
-# `./build.sh client run release` (or `./build.sh c r r`)
+# `./build.sh client run release tracy` (or `./build.sh c r rel t`)
 # `./build.sh server build debug` (or `./build.sh s b d`)
 #
 # Heads up: "release" takes time to build due to `-o:speed`. In my device it it 5x slower.
 
-## Executable (client/server)
-SRC_DIR="src/client/"
-EXE_NAME="client"
- 
-if [[ "$1" == "client" || "$1" == "c" ]]; then
-    SRC_DIR="src/client/"
-    EXE_NAME="client"
-elif [[ "$1" == "server" || "$1" == "s" ]]; then
-    SRC_DIR="src/server/"
-    EXE_NAME="server"
-else
-    echo "No executable selected. Selecting 'client' (or 'c') by default"
-    echo "Options: "
-    echo "\t 'client' (or 'c')"
-    echo "\t 'server' (or 's')"
-fi
-
-shift
-
-## Action
+## Defaults
+EXE="client"
 MODE="run"
+FLAGS="-show-timings"
+ENABLE_TRACY=false
+BUILD_TYPE="" # no need for super fast release
 
-if [[ "$1" == "run" || "$1" == "r" ]]; then
-    MODE="run"
-elif [[ "$1" == "build" || "$1" == "b" ]]; then
-    MODE="build"
-else
-    echo "No action selected. Selecting 'run' (or 'r') by default"
-    echo "Options: "
-    echo "\t 'run' (or 'r')"
-    echo "\t 'build' (or 'b')"
+for arg in "$@"; do
+    case $arg in
+        client|c)    EXE="client" ;;
+        server|s)    EXE="server" ;;
+        run|r)       MODE="run" ;;
+        build|b)     MODE="build" ;;
+        release|rel) BUILD_TYPE="release" ;;
+        debug|d)     BUILD_TYPE="debug" ;;
+        tracy|t)     ENABLE_TRACY=true ;;
+    esac
+done
+
+OUT_DIR="bin/$BUILD_TYPE/$EXE"
+
+if [[ "$EXE" == "client" ]]; then
+    SRC_DIR="src/client/"
+elif [[ "$EXE" == "server" ]]; then
+    SRC_DIR="src/server/"
 fi
 
-shift
-
-## Build type
-FLAGS="-show-timings"
-OUT_DIR="bin/" # no output when running
-
-if [[ "$1" == "release" || "$1" == "r" ]]; then
+if [[ "$BUILD_TYPE" == "release" ]]; then
     # FLAGS+="-microarch:native"
     FLAGS+=" -o:speed"
-    OUT_DIR+="release/"
-elif [[ "$1" == "debug" || "$1" == "d" ]]; then
+elif [[ "$BUILD_TYPE" == "debug" ]]; then
     FLAGS+=" -debug"
-    OUT_DIR+="debug/"
-else
-    echo "No build type selected. Selecting 'release' (or 'r') by default"
-    echo "Options: "
-    echo "\t 'release' (or 'r')"
-    echo "\t 'debug' (or 'd')"
 fi
 
-OUT_DIR+="bs-odin" # debug/bs-odin or release/bs-odin
-
-shift
-
 # Build Tracy
-ENABLE_TRACY=true
-
 if [[ "$ENABLE_TRACY" == true ]]; then
     if [ ! -f "thirdparty/tracy/tracy.so" ]; then
         echo "Building Tracy library"
         (cd "thirdparty/tracy/" && c++ -DTRACY_ENABLE -O2 tracy/public/TracyClient.cpp -shared -fPIC -o tracy.so)
-    else
-        echo "Tracy Already Built"
     fi
     FLAGS+=" -define:TRACY_ENABLE=true"
 else
@@ -85,6 +58,6 @@ COLLECTION="-collection:src=./src/"
 COLLECTION+=" -collection:thirdparty=./thirdparty/"
 
 # RUN ODIN!!
-echo "odin $MODE src/client/ $COLLECTION $FLAGS -out:$OUT_DIR"
+echo "odin $MODE $SRC_DIR $COLLECTION $FLAGS -out:$OUT_DIR"
 
-odin $MODE src/client/ $COLLECTION $FLAGS -out:$OUT_DIR
+odin $MODE $SRC_DIR $COLLECTION $FLAGS -out:$OUT_DIR
