9008d36d1d3bd45d21fd685188d269f9ab858d62
Ankush Roy <ankush3411111@gmail.com>
2026-07-28 22:21:22 +0530

    Feat: Fix the shader to output more smoother color

--- res/shaders/glsl330/terrain.fs
+++ res/shaders/glsl330/terrain.fs
@@ -13,11 +13,12 @@ void main() {
         
         float height = (tex_val * 3.0) - 1.5; 
         
-        float darkness = clamp((height + 1.5) / 1.5, 0.6, 1.0);
+        float blend = smoothstep(-1.0, -0.3, height);
         
-        vec3 waterBase = vec3(50.0, 162.0, 230.0) / 255.0;
+        vec3 water = vec3(50.0, 162.0, 230.0) / 255.0;
+        vec3 waterDark = vec3(34, 115, 163) / 255.0;
 
-        finalColor = vec4(waterBase * darkness, 1.0);
+        finalColor = vec4(mix(waterDark, water, blend), 1.0);
     } else {
         finalColor = fragColor;
     }
